d0b0128bdf3708deeeeb00acc4de593e8735bda2
svyatoslavPy <svyatoslavik9@gmail.com>
2022-11-29 17:18:26 +0300

    creating index.html and js/main.js inside task1

---
+++ task1/js/main.js
@@ -0,0 +1,18 @@
+// task1 - script, which console.log 3 different ones and a greeting in the console type.
+
+let numbers = 123456; // return type number
+let nameSchool = "IT School 'Hillel'"; // return type string
+let boolean = true; // return boolean
+
+// variable typeof
+let resultNumbers = typeof numbers; // number
+let resultNameschool = typeof nameSchool; // string
+let resultBoolean = typeof boolean; // boolean
+
+console.log('value: ', numbers + '; ', 'type: ', resultNumbers + ';'); // result numbers value and type
+console.log('value: ', nameSchool + '; ', 'type: ', resultNameschool + ';'); // result nameSchool value and type
+console.log('value: ', boolean + '; ', 'type: ', resultBoolean + ';'); // result boolean value and type
+
+
+
+
