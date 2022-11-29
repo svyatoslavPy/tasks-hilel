9f49e8e14b9043a0adacbe83e3485cda7963ffc3
svyatoslavPy <svyatoslavik9@gmail.com>
2022-11-29 20:25:50 +0300

    fix js

--- task1/js/main.js
+++ task1/js/main.js
@@ -9,9 +9,9 @@ let resultNumbers = typeof numbers; // number
 let resultNameschool = typeof nameSchool; // string
 let resultBoolean = typeof boolean; // boolean
 
-console.log('value: ' + '<' + numbers + '>' + '; ' + 'type: ' + '<' + resultNumbers + '>' + ';'); // result numbers value and type
-console.log('value: ' + '<' + nameSchool + '>' + '; ' + 'type: ' + '<' + resultNameschool + '>' + ';'); // result nameSchool value and type
-console.log('value: ' + '<' + boolean + '>' + '; ' + 'type: ' + '<' + resultBoolean + '>' + ';'); // result boolean value and type
+console.log(`value: <${numbers}>; type: <${resultNumbers}>;`); // result numbers value and type
+console.log(`value: <${nameSchool}>; type: <${resultNameschool}>;`); // result nameSchool value and type
+console.log(`value: <${boolean}>; type: <${resultBoolean}>;`); // result boolean value and type
 
 alert('Hello World'); // alert hello world
 
