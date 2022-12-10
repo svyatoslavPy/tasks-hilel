061f83af250f5cc02ceb4a31bd32bdf21d45eed6
svyatoslavPy <svyatoslavik9@gmail.com>
2022-12-10 17:52:47 +0300

    fix js

--- task3(loop, if)/js/main.js
+++ task3(loop, if)/js/main.js
@@ -1,83 +1,85 @@
 // task 1
-
-let userName = prompt('Enter to username: '); // username
-let userStr = '';
+let userName = prompt('Enter to username: ') // username
+let userStr = ''
 
 for (let i = 0; i <= 3; i++) {
 	if (i == 2) {
-		userStr += `Happy birthday, dear ${userName}\n`;
+		userStr += `Happy birthday, dear ${userName}\n`
 	} else {
-		userStr += 'Happy birthday to you\n';
+		userStr += 'Happy birthday to you\n'
 	}
 }
-console.log(userStr);
+console.log(userStr)
+
 
 // task 2
-let repeatWord = prompt('Number of repeat: '); // number of repeat
-let resultWord = ''; // word
+let repeatWord = prompt('Number of repeat: ') // number of repeat
+let resultWord = '' // word
 
 for (let i = 0; i < repeatWord; i++) {
 	// loop
-	resultWord += '.#'; // addition .# in resultWord
+	resultWord += '.#' // addition .# in resultWord
 }
 
-console.log(resultWord);
+console.log(resultWord)
 // resultWord
 
+
 // task 3
-let number = +prompt('Enter to number: ');
-let sum = 0;
+let number = +prompt('Enter to number: ')
+let sum = 0
 
 if (isNaN(number) || number === '') {
-	number = +prompt('Invalid. You should enter a number: ');
+	number = +prompt('Invalid. You should enter a number: ')
 }
 
 for (let i = 0; i < number; i++) {
 	if (i % 2 !== 0) {
-		sum += i;
+		sum += i
 	}
 }
-console.log(sum);
+console.log(sum)
 // sum
 
-// task 4
 
-let numberRandom = 0;
-let iteration = 0;
+// task 4
+let numberRandom = 0
+let iteration = 0
 
 while (true) {
-	numberRandom = Math.random();
-	iteration++;
+	numberRandom = Math.random()
+	iteration++
 
 	if (numberRandom > 0.7) {
-		console.log(`Loop was finished because of: ${numberRandom}`);
-		break;
+		console.log(`Loop was finished because of: ${numberRandom}`)
+		break
 	}
 
-	console.log(`Number of iterations: ${iteration}`);
+	console.log(`Number of iterations: ${iteration}`)
 }
 
 
 // task 5
-
 for (let i = 0; i <= 50; i++) {
 	// console.log(`from 1 to 50: ${i}`);
 
 	if (i % 3 == 0) {
-		console.log('Div by 3');
+		console.log('Div by 3')
 	} else if (i % 5 == 0) {
-		console.log('Div by 5');
+		console.log('Div by 5')
 	} else if (i % 3 == 0 && i % 5 == 0) {
-		console.log('Div by 5 and 3, 4');
+		console.log('Div by 5 and 3, 4')
 	} else {
-		console.log(`Other numbers: ${i}`);
+		console.log(`Other numbers: ${i}`)
 	}
 }
 
 
+
 // task 6
+console.log('====> data and time: ');
+let data = new Date()
 
-
-let data = new Date();
-
-console.log(`Data: ${data.getUTCDate()}, Time: ${data.getUTCHours()}:${data.getUTCMinutes()}`);
+console.log(`date = ${data.getDate()}`);
+console.log(`year = ${data.getFullYear()}`)
+console.log(`time = ${data.getHours()}:${data.getMinutes()}`)
