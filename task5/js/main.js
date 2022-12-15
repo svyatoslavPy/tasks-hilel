e58aaee6d38a269bf48213251e0be2c866d7d2a6
svyatoslavPy <svyatoslavik9@gmail.com>
2022-12-15 17:34:34 +0300

    fix js

--- task5/js/main.js
+++ task5/js/main.js
@@ -1,32 +1,32 @@
-// // task1 
-// const fillArray = (num, word) => {
+// task1 
+const fillArray = (num, word) => {
 
-// 	let arr = [];
+	let arr = [];
 
-// 	for (let i = 0; i < num; i++) {
-// 		arr[i] = word;
-// 	}
-// 	return arr;
-// }
+	for (let i = 0; i < num; i++) {
+		arr[i] = word;
+	}
+	return arr;
+}
 
-// console.log(fillArray(3, 'qwerty'));
+console.log(fillArray(3, 'qwerty'));
 
 
 
-// // task 2
-// let filterArr = [0, 1, 2, null, undefined, 'qwerty', false];
+// task 2
+let filterArr = [0, 1, 2, null, undefined, 'qwerty', false];
 
-// const filterArray = (a, b, c, d, e) => {
+const filterArray = (a, b, c, d, e) => {
 	
-// 	for (let i = 0; i < filterArr.length; i++) {
+	for (let i = 0; i < filterArr.length; i++) {
 
-// 		if (filterArr[i] !== a && filterArr[i] !== b && filterArr[i] !== c && filterArr[i] !== d && filterArr[i] !== e) {
-// 			console.log(filterArr[i]);
-// 		}
-// 	}
-// }
+		if (filterArr[i] !== a && filterArr[i] !== b && filterArr[i] !== c && filterArr[i] !== d && filterArr[i] !== e) {
+			console.log(filterArr[i]);
+		}
+	}
+}
 
-// filterArray(false, undefined, 'qwerty', 0, null);
+filterArray(false, undefined, 'qwerty', 0, null);
 
 
 
@@ -45,24 +45,24 @@ console.log(calcSum(1, 2, 5));
 
 
 
-// // task 4
-// const createPipe = (words) => { return `===== ${words} =====` }; 
-// console.log(createPipe('some text you like'));
+// task 4
+const createPipe = (words) => { return `===== ${words} =====` }; 
+console.log(createPipe('some text you like'));
 
 
 
 
-// // task 5
-// const textOutput = (text) => {
-// 	console.log(text);
-// }
+// task 5
+const textOutput = (text) => {
+	console.log(text);
+}
 
-// const callText = (text) => {
-// 	result = alert(text);
-// 	return result;
-// }
+const callText = (text) => {
+	result = alert(text);
+	return result;
+}
 
-// console.log(textOutput('lorem'), callText('lorem'));
+console.log(textOutput('lorem'), callText('lorem'));
 
 
 
